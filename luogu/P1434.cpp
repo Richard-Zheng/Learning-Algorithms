20ebb37fc65db5752cb8bfebd9b9d5241a89ae40
Richard-Zheng <yuqiu36@live.com>
2020-06-14 11:15:04 +0800

    200614 1115

--- luogu/P1434.cpp
+++ luogu/P1434.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 using namespace std;
-
-const int step[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
 int main() {
     int r, c;
     int heightAtPoint[101][101];
@@ -16,27 +13,32 @@ int main() {
         }
     }
 
-    int ans = 0;
     for (int i = 1; i <= r; ++i) {
         for (int j = 1; j <= c; ++j) {
-            dp[i][j] = max(dp[i][j], 1);
-
-            for (int dir = 0; dir < 4; ++dir) {
-                int nexti = i + step[dir][0];
-                int nextj = j + step[dir][1];
-                
-                if (nexti == 0 || nextj == 0) {
-                    continue;
-                } else if (heightAtPoint[nexti][nextj] < heightAtPoint[i][j]) {
-                    dp[nexti][nextj] = max(dp[nexti][nextj], dp[i][j] + 1);
-                    ans = max(ans, dp[nexti][nextj]);
-                } else if (heightAtPoint[nexti][nextj] > heightAtPoint[i][j]) {
-                    dp[i][j] = max(dp[i][j], dp[nexti][nextj] + 1);
-                    ans = max(ans, dp[i][j]);
+            if (i == 1) { // solution: longest increasing subsequence
+                if (heightAtPoint[i][j] < heightAtPoint[i][j - 1]) {
+                    dp[i][j] = dp[i][j - 1] + 1;
+                } else {
+                    dp[i][j] = 1;
+                }
+            } else {
+                if (heightAtPoint[i][j] < heightAtPoint[i][j - 1]) {
+                    dp[i][j] = max(dp[i][j], dp[i][j - 1] + 1);
+                }
+                if (heightAtPoint[i][j] < heightAtPoint[i - 1][j]) {
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j] + 1);
                 }
             }
         }
     }
+
+    int ans = 0;
+    for (int i = 1; i <= r; ++i) {
+        for (int j = 1; j <= c; ++j) {
+            ans = max(ans, dp[i][j]);
+        }
+    }
+
     cout << ans << endl;
     return 0;
 }
