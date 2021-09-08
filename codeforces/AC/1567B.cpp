112b5c36f3b45e05a36284ddfbcb87149e0ecac0
Richard-Zheng <yuqiu36@live.com>
2021-09-08 13:29:40 +0800

    1567B AC!

--- codeforces/AC/1567B.cpp
+++ codeforces/AC/1567B.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+int computeXOR(int n)
+{
+   
+  // If n is a multiple of 4
+  if (n % 4 == 0)
+    return n;
+ 
+  // If n%4 gives remainder 1
+  if (n % 4 == 1)
+    return 1;
+ 
+  // If n%4 gives remainder 2
+  if (n % 4 == 2)
+    return n + 1;
+ 
+  // If n%4 gives remainder 3
+  return 0;
+}
 int main() {
     int t;
     cin >> t;
@@ -8,10 +26,7 @@ int main() {
     int a, b;
     for (int i = 0; i < t; i++) {
         cin >> a >> b;
-        int axor = 0;
-        for (int j = 1; j < a; j++) {
-            axor ^= j;
-        }
+        int axor = computeXOR(a - 1);
         int k = axor ^ b;
         if (axor == b) {
             cout << a << endl;
